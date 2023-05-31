9c14655ec405a9f6d40a28526165c83fafe38634
codyh587 <108317527+codyh587@users.noreply.github.com>
2023-05-31 14:50:47 -0700

    Updated Kotlin/Firebase versions and added local.properties secrets management

--- app/src/main/java/com/example/socialphotosapp/SignUpActivity.kt
+++ app/src/main/java/com/example/socialphotosapp/SignUpActivity.kt
@@ -1,8 +1,14 @@
 package com.example.socialphotosapp
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
@@ -13,5 +19,57 @@ class SignUpActivity : AppCompatActivity() {
         signin_link_btn.setOnClickListener {
             startActivity(Intent(this, SignInActivity::class.java))
         }
+
+        signup_btn.setOnClickListener {
+            CreateAccount()
+        }
+    }
+
+    private fun CreateAccount() {
+        val fullname = fullname_signup.text.toString()
+        val username = username_signup.text.toString()
+        val email = email_signup.text.toString()
+        val password = password_signup.text.toString()
+
+        when {
+            TextUtils.isEmpty(fullname) -> Toast.makeText(this, "full name is required", Toast.LENGTH_LONG)
+            TextUtils.isEmpty(username) -> Toast.makeText(this, "username is required", Toast.LENGTH_LONG)
+            TextUtils.isEmpty(email) -> Toast.makeText(this, "email is required", Toast.LENGTH_LONG)
+            TextUtils.isEmpty(password) -> Toast.makeText(this, "password is required", Toast.LENGTH_LONG)
+
+            else -> {
+                val progressDialog = ProgressDialog(this@SignUpActivity)
+                progressDialog.setTitle("Sign Up")
+                progressDialog.setMessage("Please wait, this may take a while...")
+                progressDialog.setCanceledOnTouchOutside(false)
+                progressDialog.show()
+
+                val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+                mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            saveUserInfo(fullname, username, email)
+                        } else {
+                            val message = task.exception!!.toString()
+                            Toast.makeText(this, "Error: $message", Toast.LENGTH_LONG)
+                            mAuth.signOut()
+                            progressDialog.dismiss()
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun saveUserInfo(fullName: String, username: String, email: String) {
+        val currentUserID = FirebaseAuth.getInstance().currentUser!!.uid
+        val usersRef: DatabaseReference = FirebaseDatabase.getInstance().reference.child("Users")
+
+        val userMap = HashMap<String, Any>()
+        userMap["uid"] = currentUserID
+        userMap["fullname"] = currentUserID
+        userMap["username"] = currentUserID
+        userMap["email"] = currentUserID
+        userMap["bio"] = "I just joined. Say hello!"
+        userMap["image"] = BuildConfig.DEFAULT_IMAGE_URL
     }
 }
