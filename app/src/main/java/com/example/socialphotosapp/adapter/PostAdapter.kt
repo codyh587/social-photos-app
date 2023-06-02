813fee9ee25bf7bf20273487a65d9f5041316db7
codyh587 <108317527+codyh587@users.noreply.github.com>
2023-06-01 17:29:03 -0700

    Added following posts to Home view

---
+++ app/src/main/java/com/example/socialphotosapp/adapter/PostAdapter.kt
@@ -0,0 +1,87 @@
+package com.example.socialphotosapp.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.NonNull
+import androidx.recyclerview.widget.RecyclerView
+import com.example.socialphotosapp.R
+import com.example.socialphotosapp.model.Post
+import com.example.socialphotosapp.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
+
+class PostAdapter(private val mContext: Context,
+                  private val mPost: List<Post>): RecyclerView.Adapter<PostAdapter.ViewHolder>() {
+
+    private var firebaseUser: FirebaseUser? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(mContext).inflate(R.layout.posts_layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return mPost.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+
+        val post = mPost[position]
+        Picasso.get().load(post.getPostImage()).into(holder.postImage)
+        publisherInfo(holder.profileImage, holder.username, holder.publisher, post.getPublisher())
+    }
+
+    inner class ViewHolder(@NonNull itemView: View): RecyclerView.ViewHolder(itemView) {
+        var profileImage: CircleImageView
+        var postImage: ImageView
+        var likeButton: ImageView
+        var commentButton: ImageView
+        var saveButton: ImageView
+        var username: TextView
+        var likes: TextView
+        var publisher: TextView
+        var description: TextView
+        var comments: TextView
+
+        init {
+            profileImage = itemView.findViewById(R.id.user_profile_image_post)
+            postImage = itemView.findViewById(R.id.post_image_home)
+            likeButton = itemView.findViewById(R.id.user_profile_image_post)
+            commentButton = itemView.findViewById(R.id.user_profile_image_post)
+            saveButton = itemView.findViewById(R.id.user_profile_image_post)
+            username = itemView.findViewById(R.id.user_profile_image_post)
+            likes = itemView.findViewById(R.id.user_profile_image_post)
+            publisher = itemView.findViewById(R.id.user_profile_image_post)
+            description = itemView.findViewById(R.id.user_profile_image_post)
+            comments = itemView.findViewById(R.id.user_profile_image_post)
+        }
+    }
+
+    private fun publisherInfo(profileImage: CircleImageView, username: TextView, publisher: TextView, publisherId: String) {
+        val usersRef = FirebaseDatabase.getInstance().reference.child("Users").child(publisherId)
+
+        usersRef.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    val user = dataSnapshot.getValue<User>(User::class.java)
+                    Picasso.get().load(user!!.getImage()).placeholder(R.drawable.profile).into(profileImage)
+                    username.text = user.getUsername()
+                    publisher.text = user.getFullname()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
+    }
+}
